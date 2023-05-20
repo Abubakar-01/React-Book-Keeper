73a4379de0a46edb63b4ff3609b0ab2a26528839
abubakar <abubakaran102025@gmail.com>
2023-05-20 22:51:29 +0500

    Make create book functionality

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
+import BookCreate from "./components/BookCreate";
 
 
 function App() {
+  const [books,setBooks]=useState([]);
+  const createBook = (title) =>{
+    console.log("Need to add book with title of:",title);
+  }
   return (
-   <div>App</div>
+   <div>
+    <BookCreate onCreate={createBook} />
+   </div>
   );
 }
 
